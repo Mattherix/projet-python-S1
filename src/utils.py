e26c32b8f39711e0f08192291e8650b963673628
Mattherix <mattherix@protonmail.com>
2020-12-10 18:51:51 +0100

    Add encode_partition() to utils.py to encode a partition

--- src/utils.py
+++ src/utils.py
@@ -46,3 +46,28 @@ def decode_partition(partition, note_to_frequency, quaver):
                 figures_to_durations[note[-1]]
             ))
     return decoded_partition
+
+
+def encode_partition(partition, note_to_frequency, quaver):
+    """Encode the partition as a strings
+
+    :param partition: The partition ie: [(396, 5), (396, 2.5), (-1, 20)]
+    :param note_to_frequency: A dictionary with note associate with there frequency
+    :param quaver: The lenght of the quaver (croche)
+    :return: The encoded strings ie: "SOLc p Zb" if quaver is 5
+    """
+    figures_to_durations = get_figures_duration(quaver)
+    durations_to_figures = {duration: figure for figure, duration in figures_to_durations.items()}
+
+    frequency_to_note = {frequency: note for note, frequency in note_to_frequency.items()}
+
+    last_note = (None, 0)
+    encoded_partition = []
+    for frequency, duration in partition:
+        if frequency == last_note[0] and duration == (last_note[1] / 2):
+            encoded_partition.append('p')
+        else:
+            encoded_partition.append(frequency_to_note[frequency] + durations_to_figures[duration])
+        last_note = (frequency, duration)
+
+    return ' '.join(encoded_partition)
