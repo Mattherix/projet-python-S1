1b89450f24875739a0880aaa00011611586a54e1
Mattherix <mattherix@protonmail.com>
2021-01-05 22:14:48 +0100

    Link the interface and the play button

--- src/main.py
+++ src/main.py
@@ -12,19 +12,29 @@ label = Label(window, text="Choisiser une partion")
 label.pack()
 
 filepath = filedialog.askopenfilename(initialdir="/", title="Choisir un fichier", filetypes=(
-        ("partitions files", "*.txt"), ("all files", "*.*")))
+    ("partitions files", "*.txt"), ("all files", "*.*")))
 if not filepath:
     filepath = '../partition.txt'
 
 liste = Listbox(window)
-for i, title in enumerate(read_files(filepath)):
-    liste.insert(i + 1, title[0])
+partitions = []
+for i, title_and_partitions in enumerate(read_files(filepath)):
+    liste.insert(i + 1, title_and_partitions[0])
+    partitions.append(title_and_partitions[1])
 liste.pack()
 
 canvas = Canvas(window, bg='#EEEEEE', height=500, width=500, bd=0, highlightthickness=0)
 canvas.pack(side=TOP)
 
-btn = Button(window, text="Jouer", width=15, command=play)
+
+def get_index():
+    if liste.curselection():
+        return liste.curselection()[0]
+    else:
+        return 0
+
+
+btn = Button(window, text="Jouer", width=15, command=lambda: play(partitions[get_index()]))
 btn.pack(side=TOP, pady=5)
 btn_transposition = Checkbutton(window, text="Transposition")
 btn_invertion = Checkbutton(window, text="Invertion")
@@ -33,5 +43,5 @@ btn_invertion.pack()
 
 btn = Button(window, text="Quitter", width=15, command=window.destroy)
 btn.pack(side=BOTTOM, pady=5)
-
+print(liste.curselection())
 window.mainloop()
